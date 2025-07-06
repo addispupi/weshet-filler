447d3a4b459f2c823c1c6e32e06fbc0ab81376c7
Addisu <pupiatpixar@gmail.com>
2025-07-06 17:32:06 +0300

    docs(data): initiate profile data for random selection

---
+++ data.js
@@ -0,0 +1,20 @@
+//  Data entries for a random pick from a list of profiles
+
+const myData = {
+    profiles: [
+        {
+            firstName: "Akalu",
+            lastName: "Tasew",
+            birthDate: "1990-05-15", 
+            email: "akalu.tasew@gmail.com",
+            phone: "+251 911 234 567",
+            address: "Bole Cameron Street",
+            country: "Ethiopia",
+            city: "Addis Ababa",
+            subCity: "Bole",
+            woreda: "3",
+            houseNumber: "B472",
+            zip: "1000",
+        },
+  ]
+};
